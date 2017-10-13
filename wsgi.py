fe06e940583d1beec4235b39c2a70a2edf799251
nrot <nrot13@gmail.com>
2017-10-13 20:35:26 +0300

    edit port to try

--- wsgi.py
+++ wsgi.py
@@ -8,7 +8,7 @@ import sys
 #
 if __name__ == '__main__':
     ip   = 'localhost'
-    port = 8080
+    port = 8081
     zapp = imp.load_source('application', 'wsgi/application')
 
     from wsgiref.simple_server import make_server

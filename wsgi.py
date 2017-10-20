aef46f7e0a51a8ad6d03210fad72106821cf3e24
nrot <nrot13@gmail.com>
2017-10-20 19:36:03 +0300

    Try

--- wsgi.py
+++ wsgi.py
@@ -6,6 +6,7 @@ import os
 class MainHandler(tornado.web.RequestHandler):
 	def get(self):
 		self.write('<h1>Hello World!</h1>')
+		#self.render('index.html')
 
 def make_app():
 	return tornado.web.Application([
